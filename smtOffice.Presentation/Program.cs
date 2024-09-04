46494f520fdd733dc181c59d6d00a5529579667d
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-04 16:40:23 +0200

    Application layer

--- smtOffice.Presentation/Program.cs
+++ smtOffice.Presentation/Program.cs
@@ -1,3 +1,5 @@
+using smtOffice.Application.Extension;
+
 namespace smtOffice.Presentation
 {
     public class Program
@@ -8,6 +10,8 @@ namespace smtOffice.Presentation
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddApplication();
 
             var app = builder.Build();
 
