f55bf1d4ea7350a097626bddfd5b224752d21882
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-04 16:59:37 +0200

    Infrastructure layer

---
+++ smtoffice.Infrastructure/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace smtoffice.Infrastructure.Extension
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services)
+        {
+
+
+        }
+    }
+}
