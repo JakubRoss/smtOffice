e49fa152ea778ee35a4d9fa09105a688cbf15332
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-05 16:44:24 +0200

    Create LoginDTO.cs

---
+++ smtOffice.Application/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace smtOffice.Application.DTOs
+{
+    public class LoginDTO
+    {
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
