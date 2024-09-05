68d6487c65f1b32b55b9a04e758a846a77065629
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-05 03:08:48 +0200

    PasswordHasher

---
+++ smtoffice.Infrastructure/Common/BcryptPasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using smtOffice.Application.Interfaces;
+
+namespace smtoffice.Infrastructure.Common
+{
+    internal class BcryptPasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
