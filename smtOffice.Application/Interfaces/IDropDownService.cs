01adf0e07bdc9916b7c7f75c2ce122a2df52231c
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-05 14:27:19 +0200

    DropDownService

---
+++ smtOffice.Application/Interfaces/IDropDownService.cs
@@ -0,0 +1,9 @@
+﻿using smtOffice.Domain.Common;
+
+namespace smtOffice.Application.Services
+{
+    public interface IDropDownService<T> where T : IName
+    {
+        Task<List<string>> GetNamesFromModel(T model);
+    }
+}
