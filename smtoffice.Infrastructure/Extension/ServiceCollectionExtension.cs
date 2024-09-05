7d80dba52e85a80a3c7d832ae5f07c3d3f77f215
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-05 03:08:48 +0200

    PasswordHasher

--- smtoffice.Infrastructure/Extension/ServiceCollectionExtension.cs
+++ smtoffice.Infrastructure/Extension/ServiceCollectionExtension.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using smtoffice.Infrastructure.Common;
 using smtoffice.Infrastructure.Interfaces;
 using smtoffice.Infrastructure.Repository;
+using smtOffice.Application.Interfaces;
 using smtOffice.Application.Interfaces.Repository;
 
 namespace smtoffice.Infrastructure.Extension
@@ -28,6 +29,7 @@ namespace smtoffice.Infrastructure.Extension
             Console.WriteLine(connectionString);
             services.AddScoped<ISqlConnectionFactory>(provider => new SqlConnectionFactory(connectionString!));
             services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            services.AddTransient<IPasswordHasher, BcryptPasswordHasher>();
         }
     }
 }
