eb1d5ab1dfb4d8cc69718314e516ccbf90a5b33c
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-06 17:10:10 +0200

    EmployeeDTO.cs

--- smtOffice.Application/Maping/MapingProfile.cs
+++ smtOffice.Application/Maping/MapingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using smtOffice.Application.DTOs;
+using smtOffice.Domain.Entity;
 
 namespace smtOffice.Application.Maping
 {
@@ -6,7 +8,13 @@ namespace smtOffice.Application.Maping
     {
         public MapingProfile()
         {
-        
+            CreateMap<Employee,EmployeeDTO>()
+                .ForMember(i => i.Subdivisions, opt => opt.Ignore())
+                .ForMember(i => i.Positions, opt => opt.Ignore())
+                .ForMember(i => i.AbsenceReasons, opt => opt.Ignore())
+                .ForMember(i => i.HRManagers, opt => opt.Ignore());
+            CreateMap<EmployeeDTO, Employee>()
+                .ForMember(i => i.ID, opt => opt.Ignore());
         }
     }
 }
