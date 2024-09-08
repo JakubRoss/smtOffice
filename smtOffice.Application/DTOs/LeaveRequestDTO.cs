7c32db39fd3c1fd8c0b3819be790f37c1c03a3a6
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-09 00:55:38 +0200

    LeaveRequestDTO

---
+++ smtOffice.Application/DTOs/LeaveRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace smtOffice.Application.DTOs
+{
+    public class LeaveRequestDTO
+    {
+        public int ID { get; set; }
+        public int EmployeeID { get; set; }
+        public string AbsenceReason { get; set; } = default!;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? Comment { get; set; }
+        public string Status { get; set; } = "New";
+
+        public List<SelectListItem> AbsenceReasons { get; set; } = new List<SelectListItem>();
+    }
+}
