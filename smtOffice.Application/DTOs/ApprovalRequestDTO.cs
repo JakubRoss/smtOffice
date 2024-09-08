b2be0d3e161d37fa2f916712713b510bdfb312f6
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-09 00:34:54 +0200

    ApprovalRequestDTO

---
+++ smtOffice.Application/DTOs/ApprovalRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace smtOffice.Application.DTOs
+{
+    public class ApprovalRequestDTO
+    {
+        public int ID { get; set; }
+        public int ApproverID { get; set; }
+        public int LeaveRequestID { get; set; }
+        public string Status { get; set; } = default!;
+        public string? Comment { get; set; }
+    }
+}
