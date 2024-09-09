57a221f1d227b5f2921bbf4ab57a04f62622a38f
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-09 15:42:40 +0200

    Create LeaveApprovalController .cs with Views

---
+++ smtOffice.Presentation/Controllers/LeaveApprovalController .cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using smtOffice.Application.Interfaces;
+using System.Security.Claims;
+
+namespace smtOffice.Presentation.Controllers
+{
+    public class LeaveApprovalController : Controller
+    {
+        private readonly ILeaveApprovalCoordinatorService _leaveApprovalCoordinatorService;
+
+        public LeaveApprovalController(ILeaveApprovalCoordinatorService leaveApprovalCoordinatorService)
+        {
+            _leaveApprovalCoordinatorService = leaveApprovalCoordinatorService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                // Pobierz aktualnego pracownika
+                int approverID = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value ?? "0");
+
+                // Pobierz wnioski o zatwierdzenie z szczegółami
+                var model = await _leaveApprovalCoordinatorService.GetApprovalRequestsWithDetailsAsync(approverID);
+
+                return View(model);
+            }
+            catch (Exception )
+            {
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Details(int approvalID)
+        {
+            try
+            {
+                // Pobieranie szczegółów wniosku o zatwierdzenie i powiązanych danych
+                var model = await _leaveApprovalCoordinatorService.GetApprovalLeaveRequestDetailsAsync(approvalID);
+
+                return View(model);
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProcessRequest(int leaveRequestID, int approverID, string Comment, string action)
+        {
+            try
+            {
+                if (action == "Approve")
+                {
+                    await _leaveApprovalCoordinatorService.ApproveLeaveRequestAsync(leaveRequestID, approverID, Comment);
+                    return RedirectToAction(nameof(Index));
+                }
+                else if (action == "Reject")
+                {
+                    await _leaveApprovalCoordinatorService.RejectLeaveRequestAsync(leaveRequestID, approverID, Comment);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    throw new InvalidOperationException("Invalid action.");
+                }
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // POST: /ApprovalRequests/Submit/5
+        [HttpPost]
+        public async Task<IActionResult> Submit(int leaveRequestID)
+        {
+            try
+            {
+                if(!User.Identity.IsAuthenticated)
+                    return RedirectToAction("Index","Account");
+                int employeeID = int.Parse(User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).FirstOrDefault()!.Value);
+                await _leaveApprovalCoordinatorService.SubmitRequestAsync(leaveRequestID, employeeID);
+                return RedirectToAction(nameof(Details), new { id = leaveRequestID });
+            }
+            catch (Exception)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
