fef7da6b8fd60a62075804405c94212130b9bbd6
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-09 14:47:50 +0200

    Create ApprovalLeaveRequestDTO.cs

---
+++ smtOffice.Application/DTOs/ApprovalLeaveRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace smtOffice.Application.DTOs
+{
+    public class ApprovalLeaveRequestDTO
+    {
+        public ApprovalRequestDTO ApprovalRequest { get; set; } = new ApprovalRequestDTO();
+        public LeaveRequestDTO LeaveRequest { get; set; } = new LeaveRequestDTO();
+        public string? FullName { get; set; }
+    }
+
+}
