ba176448c8307d41a674ae53fc987dfc661f9823
Jakub Ros <Jakub.Rosploch@gmail.com>
2024-09-11 09:10:00 +0200

    layout and hove, privacy views out, added overview view with dbSchema :v:

--- smtOffice.Presentation/Program.cs
+++ smtOffice.Presentation/Program.cs
@@ -51,7 +51,7 @@ namespace smtOffice.Presentation
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Index}/{id?}");
 
             app.Run();
         }
